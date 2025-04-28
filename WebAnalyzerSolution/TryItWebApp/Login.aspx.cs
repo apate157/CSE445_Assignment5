f0cdf7510a605bd99f2f7daa64a1daef2c7d098e
apate157 <apate157@asu.edu>
2025-04-27 22:06:29 -0700

    Update Login.aspx.cs

--- WebAnalyzerSolution/TryItWebApp/Login.aspx.cs
+++ WebAnalyzerSolution/TryItWebApp/Login.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;  
+using System.Xml.Linq;
+
 
 namespace TryItWebApp
 {
@@ -11,7 +14,39 @@ namespace TryItWebApp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+           
+            var user = txtUsername.Text.Trim();
+            var pass = txtPassword.Text;
+
+            
+            var returnUrl = Request.QueryString["ReturnUrl"] ?? "Default.aspx";
+
+            
+            var xmlPath = Server.MapPath("~/Member.xml");
+            var doc     = XElement.Load(xmlPath);
+
+            
+            var match = doc.Elements("User")
+                           .FirstOrDefault(u =>
+                               (string)u.Element("Username") == user &&
+                               (string)u.Element("Password") == pass);
+
+            if (match != null)
+            {
+               
+                FormsAuthentication.SetAuthCookie(user, false);
+                Response.Redirect(returnUrl);
+            }
+            else
+            {
+               
+                lblError.Text = "Invalid credentials.";
+            }
         }
     }
 }
